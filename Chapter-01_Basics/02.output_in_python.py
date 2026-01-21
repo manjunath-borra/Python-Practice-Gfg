91d6537ff5f9457a4c69a035d62a665e4ca8c33b
Borra Manjunath <manjunathborra.dev@gmail.com>
2026-01-21 15:56:24 +0530

    up

---
+++ Chapter-01_Basics/02.output_in_python.py
@@ -0,0 +1,12 @@
+# Genrally in Python Output is done by using "print()".
+print("Hello World")
+
+# We can use the print() function to print single and multiple variables.
+# We can print multiple variables by separating them with commas.
+n = "MAN"
+print(n)
+
+n = "Sam"
+age= 50 
+place = "TG"
+print(n,age,place)
---
+++ Chapter-01_Basics/03.multiple_inputs.py
@@ -0,0 +1,6 @@
+# In Python Programming language Multiple inputs can be taken at once.
+m,f =input("Enter 2 Values :- ").split()
+print("Number of Males : ",m)
+print("Number of females : ",f)
+
+# In the above program "split()" is used to split the values among both 'm' and 'f'
