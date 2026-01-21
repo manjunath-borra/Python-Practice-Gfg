39b992d1ab311d0c2c167ea534970aad785d5305
Borra Manjunath <manjunathborra.dev@gmail.com>
2026-01-21 11:02:36 +0530

    up

---
+++ Chapter-01_Basics/01.input_in_python.py
@@ -0,0 +1,8 @@
+# In Python Programming Language User Input is given using "input()"
+# "input()" returns string type only. To Convert the return Type we should use Typecasting 
+name = input("Enter your name :- ")
+print("Hello",name,"! Welcome")
+print(type(name))
+
+#  In the above programif we give the input as "SAM" or "1234" the type of varable "name" will be "String"
+# ln5 will be used to check the type of variable "name"
