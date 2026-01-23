f835ad0243bcec321b344fa0dceb374d4a2c135d
Borra Manjunath <manjunathborra.dev@gmail.com>
2026-01-23 22:58:26 +0530

    23/1/26

---
+++ Chapter-02_Variables/02.variale_names.py
@@ -0,0 +1,9 @@
+# Valid Names
+age = 21
+_colour = "lilac"
+total_score = 90
+print(age,_colour,total_score)
+# InValid Names
+# 1name = "Error"  # Starts with a digit
+# class = 10       # 'class' is a reserved keyword
+# user-name = "Doe"  # Contains a hyphen
---
+++ Chapter-02_Variables/03.Variable_assesement.py
@@ -0,0 +1,5 @@
+# Variable Assesment 
+x = 5
+y = 3.14
+z = "Hi"
+print(x,y,z)
